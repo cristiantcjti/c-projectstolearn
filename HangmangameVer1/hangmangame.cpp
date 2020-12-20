6bd03f0549bb905c68159fe9702ea8f246a22018
Cristian <cristiantcjti@gmail.com>
2020-12-19 21:05:44 -0300

    Starting the game

--- HangmangameVer1/hangmangame.cpp
+++ HangmangameVer1/hangmangame.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+//A brief introduction
+//Msg : Select the level (Three levels), select the level's words, Sort the words selected and store the words in an array.
+//Msg : Select a word. 
+int level;
+char wordsLevel1[3]={'door','car','house'};
+char wordsLevel2[3]={'study','practice','thoughtful'};
+char wordsLevel3[3]={'entertainment','householder','thoroughfare'};
+
 int main(){
-    cout << "Hello hangmangame";
-    cout << "Hello debugger";
+    cout << "Select a round level:[E]=Easy [M]=Medium [A]=Advanced";
+    cin >>level;
     return 0;
 }
